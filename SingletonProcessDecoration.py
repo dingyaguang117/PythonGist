92196f903d474d843873ed3270c13c14dbe77795
dingyaguang117 <dingyaguang117@126.com>
2013-05-20 10:54:15 +0800

    Create SingletonProcessDecoration.py

---
+++ SingletonProcessDecoration.py
@@ -0,0 +1,24 @@
+import os
+
+def SingletonProcessDecoration(filename):
+    def _deco(func):
+        def __deco(*args, **kwargs):
+            if os.path.exists(filename):
+                print 'lockfile %s exists..program exit...'%filename
+                return
+            open(filename,'w').close()
+            ret = func(*args, **kwargs)
+            os.remove(filename)
+            return ret
+        return __deco
+    return _deco
+
+
+@SingletonProcessDecoration('1.lock')
+def main():
+    import time
+    time.sleep(3)
+    return True
+    
+if __name__ == '__main__':
+    print main()
