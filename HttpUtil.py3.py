5b5cecfd6f6be0a60dd77bc3d7b64e8ede1de04f
dingyaguang117 <dingyaguang117@gmail.com>
2013-11-06 12:00:55 +0800

    Update HttpUtil.py3.py

--- HttpUtil.py3.py
+++ HttpUtil.py3.py
@@ -8,16 +8,16 @@ import gzip
 
 
 class HttpUtil():
-    def __init__(self,proxy = None, headers = None):
+    def __init__(self,headers = None,proxy = None):
         #proxy = {'http': 'http://210.14.143.53:7620'}
         if proxy != None:
-            pass
             proxy_handler = urllib.request.ProxyHandler(proxy)
             self.opener = urllib.request.build_opener(proxy_handler,urllib.request.HTTPCookieProcessor(CookieJar()))
         else:
-            pass
             self.opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(CookieJar()))
-        if not headers:
+        if headers:
+            self.opener.addheaders = headers
+        else:
             self.opener.addheaders=[('User-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11'),\
                                 ]
 
@@ -31,9 +31,7 @@ class HttpUtil():
                 time.sleep(1)
                 print(traceback.format_exc())
                 continue
-        
         return None
-    
     def Post(self,url,data,times=1):
         for i in range(times):
             try:
@@ -44,7 +42,7 @@ class HttpUtil():
                 print(traceback.format_exc())
                 continue
         return None
-    
+
     def real_url(self,url,times=1):
         for i in range(times):
             try:
@@ -54,7 +52,7 @@ class HttpUtil():
                 print(traceback.format_exc())
                 continue
         return None
-    
+
     def unzip(self,data):
         import gzip
         from io import StringIO
@@ -64,9 +62,8 @@ class HttpUtil():
         gz.close()
         return data
 
-        
+
 if __name__ =='__main__':
     httpUtil = HttpUtil()
     content = httpUtil.Get('http://lol.duowan.com/1108/m_178050471525.html')
-    print(httpUtil.unzip(content))
-    print(len(content))
+    print(content)
