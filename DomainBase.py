ffdabb2351dcfc0798394453fe2e17ad55c8c71e
dingyaguang117 <dingyaguang117@gmail.com>
2014-05-08 10:30:18 +0800

    Update DomainBase.py

--- DomainBase.py
+++ DomainBase.py
@@ -12,21 +12,34 @@ PS：后面带默认值，且赋值的时候不做类型检查
 
 class DomainBase():
     def __init__(self):
-        self.data = {}
-        
+        #注意这里，如果用 self.data = {} 会触发__setattr__导致非预期效果
+        #这里把成员存储在名为data的字典里面而不是直接存在 __dict__的原因是，会导致循环递归爆栈
+        self.__dict__['data'] = {}
+
     def __setitem__(self,key,value):
         if key not in self.__class__.__dict__:
             raise AttributeError('%s not found'%key)
         self.data[key] = value
-    
+
     def __getitem__(self,key):
         if key in self.data:
             return self.data[key]
         if key not in self.__class__.__dict__:
             raise AttributeError('%s not found'%key)
         return self.__class__.__dict__[key]
-    
-    
+
+    def __setattr__(self,key,value):
+        if key not in self.__class__.__dict__:
+            raise AttributeError('%s not found'%key)
+        self.data[key] = value
+
+    def __getattr__(self,key):
+        if key in self.data:
+            return self.data[key]
+        if key not in self.__class__.__dict__:
+            raise AttributeError('%s not found'%key)
+        return self.__class__.__dict__[key]
+
     def getInsertDict(self):
         ret = {}
         for key in self.__class__.__dict__:
@@ -36,10 +49,18 @@ class DomainBase():
             else:
                 ret[key] = self.data[key]
         return ret
-    
+
     def getUpdateDict(self):
         return self.data
 
+    def update(self, data, **kwargs):
+        for key in kwargs:
+            data[key] = kwargs[key]
+        self.data.update(data)
+        return self
+
+    def pop(self, key):
+        return self.data.pop(key)
 
 
 
@@ -52,3 +73,5 @@ class Resource(DomainBase):
     duration = -1
     channelId = -1
     categoryId = -1
+
+
